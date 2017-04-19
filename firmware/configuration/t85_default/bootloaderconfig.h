399c24c86a4971a73cd22c2414a7d7bf759081c2
Denisse Suarez <dsuarez@thoughtworks.com>
2017-04-19 12:39:32 -0300

    Edit make file and bootladerconfig

--- firmware/configuration/t85_default/bootloaderconfig.h
+++ firmware/configuration/t85_default/bootloaderconfig.h
@@ -1,12 +1,12 @@
 /* Name: bootloaderconfig.h
- * Micronucleus configuration file. 
+ * Micronucleus configuration file.
  * This file (together with some settings in Makefile.inc) configures the boot loader
  * according to the hardware.
- * 
+ *
  * Controller type: ATtiny 85 - 16.5 MHz
  * Configuration:   Default configuration
- *       USB D- :   PB3
- *       USB D+ :   PB4
+ *       USB D- :   PB0
+ *       USB D+ :   PB1
  *       Entry  :   Always
  *       LED    :   None
  *       OSCCAL :   Stays at 16 MHz
@@ -28,11 +28,11 @@
    * "B", the registers PORTB, PINB and DDRB will be used.
    */
 
-#define USB_CFG_DMINUS_BIT      3
+#define USB_CFG_DMINUS_BIT      0
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
-#define USB_CFG_DPLUS_BIT       4
+#define USB_CFG_DPLUS_BIT       1
 /* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
  * This may be any bit in the port, but must be configured as a pin change interrupt.
  */
@@ -78,11 +78,11 @@
 
 /*
  *  Define Bootloader entry condition
- * 
+ *
  *  If the entry condition is not met, the bootloader will not be activated and the user program
  *  is executed directly after a reset. If no user program has been loaded, the bootloader
  *  is always active.
- * 
+ *
  *  ENTRY_ALWAYS        Always activate the bootloader after reset. Requires the least
  *                      amount of code.
  *
@@ -90,34 +90,34 @@
  *                      to enter the bootloader from the user program.
  *                      Adds 22 bytes.
  *
- *  ENTRY_EXT_RESET     Activate the bootloader after an external reset was issued by 
+ *  ENTRY_EXT_RESET     Activate the bootloader after an external reset was issued by
  *                      pulling the reset pin low. It may be necessary to add an external
  *                      pull-up resistor to the reset pin if this entry method appears to
  *                      behave unreliably.
  *                      Adds 22 bytes.
  *
- *  ENTRY_JUMPER        Activate the bootloader when a specific pin is pulled low by an 
- *                      external jumper. 
+ *  ENTRY_JUMPER        Activate the bootloader when a specific pin is pulled low by an
+ *                      external jumper.
  *                      Adds 34 bytes.
  *
  *       JUMPER_PIN     Pin the jumper is connected to. (e.g. PB0)
- *       JUMPER_PORT    Port out register for the jumper (e.g. PORTB)  
- *       JUMPER_DDR     Port data direction register for the jumper (e.g. DDRB)  
- *       JUMPER_INP     Port inout register for the jumper (e.g. PINB)  
- * 
+ *       JUMPER_PORT    Port out register for the jumper (e.g. PORTB)
+ *       JUMPER_DDR     Port data direction register for the jumper (e.g. DDRB)
+ *       JUMPER_INP     Port inout register for the jumper (e.g. PINB)
+ *
  */
 
 #define ENTRYMODE ENTRY_ALWAYS
 
 #define JUMPER_PIN    PB0
-#define JUMPER_PORT   PORTB 
-#define JUMPER_DDR    DDRB 
-#define JUMPER_INP    PINB 
- 
+#define JUMPER_PORT   PORTB
+#define JUMPER_DDR    DDRB
+#define JUMPER_INP    PINB
+
 /*
   Internal implementation, don't change this unless you want to add an entrymode.
-*/ 
- 
+*/
+
 #define ENTRY_ALWAYS    1
 #define ENTRY_WATCHDOG  2
 #define ENTRY_EXT_RESET 3
@@ -136,7 +136,7 @@
   #define bootLoaderExit()
   #define bootLoaderStartCondition() (MCUSR&_BV(EXTRF))
 #elif ENTRYMODE==ENTRY_JUMPER
-  // Enable pull up on jumper pin and delay to stabilize input    
+  // Enable pull up on jumper pin and delay to stabilize input
   #define bootLoaderInit()   {JUMPER_DDR&=~_BV(JUMPER_PIN);JUMPER_PORT|=_BV(JUMPER_PIN);_delay_ms(1);}
   #define bootLoaderExit()   {JUMPER_PORT&=~_BV(JUMPER_PIN);}
   #define bootLoaderStartCondition() (!(JUMPER_INP&_BV(JUMPER_PIN)))
@@ -145,10 +145,10 @@
 #endif
 
 /*
- * Define bootloader timeout value. 
- * 
+ * Define bootloader timeout value.
+ *
  *  The bootloader will only time out if a user program was loaded.
- * 
+ *
  *  AUTO_EXIT_NO_USB_MS        The bootloader will exit after this delay if no USB is connected.
  *                             Set to 0 to disable
  *                             Adds ~6 bytes.
@@ -157,16 +157,16 @@
  *  AUTO_EXIT_MS               The bootloader will exit after this delay if no USB communication
  *                             from the host tool was received.
  *                             Set to 0 to disable
- *  
+ *
  *  All values are approx. in milliseconds
  */
 
 #define AUTO_EXIT_NO_USB_MS    0
-#define AUTO_EXIT_MS           6000
+#define AUTO_EXIT_MS           1000
 
  /*
  *  Defines the setting of the RC-oscillator calibration after quitting the bootloader. (OSCCAL)
- * 
+ *
  *  OSCCAL_RESTORE_DEFAULT    Set this to '1' to revert to OSCCAL factore calibration after bootlaoder exit.
  *                            This is 8 MHz +/-2% on most devices or 16 MHz on the ATtiny 85 with activated PLL.
  *                            Adds ~14 bytes.
@@ -186,60 +186,13 @@
  *
  *  If no option is selected, OSCCAL will be left untouched and stays at either factory calibration or F_CPU depending
  *  on whether the bootloader was activated. This will take the least memory. You can use this if your program
- *  comes with its own OSCCAL calibration or an external clock source is used. 
+ *  comes with its own OSCCAL calibration or an external clock source is used.
  */
- 
+
 #define OSCCAL_RESTORE_DEFAULT 0
 #define OSCCAL_SAVE_CALIB 1
 #define OSCCAL_HAVE_XTAL 0
   
-/*  
- *  Defines handling of an indicator LED while the bootloader is active.  
- * 
- *  LED_MODE                  Define behavior of attached LED or suppress LED code.
- *
- *          NONE              Do not generate LED code (gains 18 bytes).
- *          ACTIVE_HIGH       LED is on when output pin is high. This will toggle bettwen 1 and 0.
- *          ACTIVE_LOW        LED is on when output pin is low.  This will toggle between Z and 0.
- *
- *  LED_DDR,LED_PORT,LED_PIN  Where is your LED connected?
- *
- */ 
-
-#define LED_MODE    NONE
-
-#define LED_DDR     DDRB
-#define LED_PORT    PORTB
-#define LED_PIN     PB1
-
-/*
- *  This is the implementation of the LED code. Change the configuration above unless you want to 
- *  change the led behavior
- *
- *  LED_INIT                  Called once after bootloader entry
- *  LED_EXIT                  Called once during bootloader exit
- *  LED_MACRO                 Called in the main loop with the idle counter as parameter.
- *                            Use to define pattern.
-*/
-
-#define NONE        0
-#define ACTIVE_HIGH 1
-#define ACTIVE_LOW  2
-
-#if LED_MODE==ACTIVE_HIGH
-  #define LED_INIT(x)   LED_DDR   |= _BV(LED_PIN); 
-  #define LED_EXIT(x)   {LED_DDR  &=~_BV(LED_PIN);LED_PORT  &=~_BV(LED_PIN);}
-  #define LED_MACRO(x)  if ( x & 0x4c ) {LED_PORT&=~_BV(LED_PIN);} else {LED_PORT|=_BV(LED_PIN);}
-#elif LED_MODE==ACTIVE_LOW
-  #define LED_INIT(x)   LED_PORT &=~_BV(LED_PIN);   
-  #define LED_EXIT(x)   LED_DDR  &=~_BV(LED_PIN);
-  #define LED_MACRO(x)  if ( x & 0x4c ) {LED_DDR&=~_BV(LED_PIN);} else {LED_DDR|=_BV(LED_PIN);}  
-#elif LED_MODE==NONE
-  #define LED_INIT(x)
-  #define LED_EXIT(x)
-  #define LED_MACRO(x)
-#endif
-
 /* --------------------------------------------------------------------------- */
 /* Micronucleus internal configuration. Do not change anything below this line */
 /* --------------------------------------------------------------------------- */
@@ -256,4 +209,4 @@
 #define POSTSCRIPT_SIZE 6
 #define PROGMEM_SIZE (BOOTLOADER_ADDRESS - POSTSCRIPT_SIZE) /* max size of user program */
 
-#endif /* __bootloader_h_included__ */
+	#endif /* __bootloader_h_included__ */
--- firmware/main.c
+++ firmware/main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Project: Micronucleus -  v2.3
  *
  * Micronucleus V2.3             (c) 2016 Tim Bo"scke - cpldcpu@gmail.com
@@ -10,7 +10,7 @@
  *
  * License: GNU GPL v2 (see License.txt)
  */
- 
+
 #define MICRONUCLEUS_VERSION_MAJOR 2
 #define MICRONUCLEUS_VERSION_MINOR 3
 
@@ -24,14 +24,14 @@
 #include "usbdrv/usbdrv.c"
 
 // verify the bootloader address aligns with page size
-#if (defined __AVR_ATtiny841__)||(defined __AVR_ATtiny441__)  
+#if (defined __AVR_ATtiny841__)||(defined __AVR_ATtiny441__)
   #if BOOTLOADER_ADDRESS % ( SPM_PAGESIZE * 4 ) != 0
     #error "BOOTLOADER_ADDRESS in makefile must be a multiple of chip's pagesize"
   #endif
 #else
   #if BOOTLOADER_ADDRESS % SPM_PAGESIZE != 0
     #error "BOOTLOADER_ADDRESS in makefile must be a multiple of chip's pagesize"
-  #endif  
+  #endif
 #endif
 
 #if SPM_PAGESIZE>256
@@ -45,13 +45,13 @@
 // Device configuration reply
 // Length: 6 bytes
 //   Byte 0:  User program memory size, high byte
-//   Byte 1:  User program memory size, low byte   
+//   Byte 1:  User program memory size, low byte
 //   Byte 2:  Flash Pagesize in bytes
-//   Byte 3:  Page write timing in ms. 
+//   Byte 3:  Page write timing in ms.
 //    Bit 7 '0': Page erase time equals page write time
 //    Bit 7 '1': Page erase time equals page write time divided by 4
 //   Byte 4:  SIGNATURE_1
-//   Byte 5:  SIGNATURE_2 
+//   Byte 5:  SIGNATURE_2
 
 PROGMEM const uint8_t configurationReply[6] = {
   (((uint16_t)PROGMEM_SIZE) >> 8) & 0xff,
@@ -60,23 +60,23 @@ PROGMEM const uint8_t configurationReply[6] = {
   MICRONUCLEUS_WRITE_SLEEP,
   SIGNATURE_1,
   SIGNATURE_2
-};  
+};
 
   typedef union {
     uint16_t w;
     uint8_t b[2];
   } uint16_union_t;
-  
+
 #if OSCCAL_RESTORE_DEFAULT
   register uint8_t      osccal_default  asm("r2");
-#endif 
+#endif
 
-register uint16_union_t currentAddress  asm("r4");  // r4/r5 current progmem address, used for erasing and writing 
+register uint16_union_t currentAddress  asm("r4");  // r4/r5 current progmem address, used for erasing and writing
 register uint16_union_t idlePolls       asm("r6");  // r6/r7 idlecounter
 
 // command system schedules functions to run in the main loop
 enum {
-  cmd_local_nop=0, 
+  cmd_local_nop=0,
   cmd_device_info=0,
   cmd_transfer_page=1,
   cmd_erase_application=2,
@@ -84,7 +84,7 @@ enum {
   cmd_exit=4,
   cmd_write_page=64  // internal commands start at 64
 };
-register uint8_t        command         asm("r3");  // bind command to r3 
+register uint8_t        command         asm("r3");  // bind command to r3
 
 // Definition of sei and cli without memory barrier keyword to prevent reloading of memory variables
 #define sei() asm volatile("sei")
@@ -93,7 +93,7 @@ register uint8_t        command         asm("r3");  // bind command to r3
 #define wdr() asm volatile("wdr")
 
 // Use the old delay routines without NOP padding. This saves memory.
-#define __DELAY_BACKWARD_COMPATIBLE__   
+#define __DELAY_BACKWARD_COMPATIBLE__
 
 /* ------------------------------------------------------------------------ */
 static inline void eraseApplication(void);
@@ -112,16 +112,16 @@ static inline void eraseApplication(void) {
   uint16_t ptr = BOOTLOADER_ADDRESS;
 
   while (ptr) {
-#if (defined __AVR_ATtiny841__)||(defined __AVR_ATtiny441__)    
-    ptr -= SPM_PAGESIZE * 4;        
+#if (defined __AVR_ATtiny841__)||(defined __AVR_ATtiny441__)
+    ptr -= SPM_PAGESIZE * 4;
 #else
-    ptr -= SPM_PAGESIZE;        
-#endif    
+    ptr -= SPM_PAGESIZE;
+#endif
     boot_page_erase(ptr);
   }
-  
+
   // Reset address to ensure the reset vector is written first.
-  currentAddress.w = 0;   
+  currentAddress.w = 0;
 }
 
 // simply write currently stored page in to already erased flash memory
@@ -132,11 +132,11 @@ static inline void writeFlashPage(void) {
 
 // Write a word into the page buffer.
 // Will patch the bootloader reset vector into the main vectortable to ensure
-// the device can not be bricked. Saving user-reset-vector is done in the host 
+// the device can not be bricked. Saving user-reset-vector is done in the host
 // tool, starting with firmware V2
 static void writeWordToPageBuffer(uint16_t data) {
 
-#ifndef ENABLE_UNSAFE_OPTIMIZATIONS     
+#ifndef ENABLE_UNSAFE_OPTIMIZATIONS
   #if BOOTLOADER_ADDRESS < 8192
   // rjmp
   if (currentAddress.w == RESET_VECTOR_OFFSET * 2) {
@@ -148,16 +148,16 @@ static void writeWordToPageBuffer(uint16_t data) {
     data = 0x940c;
   } else if (currentAddress.w == (RESET_VECTOR_OFFSET +1 ) * 2) {
     data = (BOOTLOADER_ADDRESS/2);
-  }    
+  }
   #endif
 #endif
 
 #if OSCCAL_SAVE_CALIB
    if (currentAddress.w == BOOTLOADER_ADDRESS - TINYVECTOR_OSCCAL_OFFSET) {
       data = OSCCAL;
-   }     
+   }
 #endif
-  
+
   boot_page_fill(currentAddress.w, data);
   currentAddress.w += 2;
 }
@@ -165,52 +165,52 @@ static void writeWordToPageBuffer(uint16_t data) {
 /* ------------------------------------------------------------------------ */
 static uint8_t usbFunctionSetup(uint8_t data[8]) {
   usbRequest_t *rq = (void *)data;
- 
+
   if (rq->bRequest == cmd_device_info) { // get device info
     usbMsgPtr = (usbMsgPtr_t)configurationReply;
-    return sizeof(configurationReply);      
-  } else if (rq->bRequest == cmd_transfer_page) { 
+    return sizeof(configurationReply);
+  } else if (rq->bRequest == cmd_transfer_page) {
       // Set page address. Address zero always has to be written first to ensure reset vector patching.
       // Mask to page boundary to prevent vulnerability to partial page write "attacks"
         if ( currentAddress.w != 0 ) {
-            currentAddress.b[0]=rq->wIndex.bytes[0] & (~ (SPM_PAGESIZE-1));     
-            currentAddress.b[1]=rq->wIndex.bytes[1];     
-            
-            // clear page buffer as a precaution before filling the buffer in case 
-            // a previous write operation failed and there is still something in the buffer.         
+            currentAddress.b[0]=rq->wIndex.bytes[0] & (~ (SPM_PAGESIZE-1));
+            currentAddress.b[1]=rq->wIndex.bytes[1];
+
+            // clear page buffer as a precaution before filling the buffer in case
+            // a previous write operation failed and there is still something in the buffer.
             __SPM_REG=(_BV(CTPB)|_BV(__SPM_ENABLE));
             asm volatile("spm");
-            
-        }        
+
+        }
     } else if (rq->bRequest == cmd_write_data) { // Write data
       writeWordToPageBuffer(rq->wValue.word);
       writeWordToPageBuffer(rq->wIndex.word);
       if ((currentAddress.b[0] % SPM_PAGESIZE) == 0)
-          command=cmd_write_page; // ask runloop to write our page       
+          command=cmd_write_page; // ask runloop to write our page
   } else {
     // Handle cmd_erase_application and cmd_exit
-    command=rq->bRequest&0x3f;    
+    command=rq->bRequest&0x3f;
   }
   return 0;
 }
 
 static void initHardware (void)
 {
-  // Disable watchdog and set timeout to maximum in case the WDT is fused on 
+  // Disable watchdog and set timeout to maximum in case the WDT is fused on
 #ifdef CCP
   // New ATtinies841/441 use a different unlock sequence and renamed registers
-  MCUSR=0;    
-  CCP = 0xD8; 
-  WDTCSR = 1<<WDP2 | 1<<WDP1 | 1<<WDP0; 
+  MCUSR=0;
+  CCP = 0xD8;
+  WDTCSR = 1<<WDP2 | 1<<WDP1 | 1<<WDP0;
 #else
-  MCUSR=0;    
+  MCUSR=0;
   WDTCR = 1<<WDCE | 1<<WDE;
-  WDTCR = 1<<WDP2 | 1<<WDP1 | 1<<WDP0; 
-#endif  
+  WDTCR = 1<<WDP2 | 1<<WDP1 | 1<<WDP0;
+#endif
 
-  
+
   usbDeviceDisconnect();  /* do this while interrupts are disabled */
-  _delay_ms(300);  
+  _delay_ms(300);
   usbDeviceConnect();
 
   usbInit();    // Initialize INT settings after reconnect
@@ -220,30 +220,30 @@ static void initHardware (void)
 // reset system to a normal state and launch user program
 static void leaveBootloader(void) __attribute__((__noreturn__));
 static inline void leaveBootloader(void) {
- 
+
   bootLoaderExit();
 
 #if OSCCAL_RESTORE_DEFAULT
   OSCCAL=osccal_default;
   nop(); // NOP to avoid CPU hickup during oscillator stabilization
 #endif
-    
+
  asm volatile ("rjmp __vectors - 4"); // jump to application reset vector at end of flash
-  
+
  for (;;); // Make sure function does not return to help compiler optimize
 }
 
 void USB_INTR_VECTOR(void);
 int main(void) {
   uint8_t osccal_tmp;
-  
+
   bootLoaderInit();
-  
+
   /* save default OSCCAL calibration  */
 #if OSCCAL_RESTORE_DEFAULT
   osccal_default = OSCCAL;
 #endif
-  
+
 #if OSCCAL_SAVE_CALIB
   // adjust clock to previous calibration value, so bootloader starts with proper clock calibration
   unsigned char stored_osc_calibration = pgm_read_byte(BOOTLOADER_ADDRESS - TINYVECTOR_OSCCAL_OFFSET);
@@ -252,126 +252,121 @@ int main(void) {
     nop();
   }
 #endif
-  
+
   if (bootLoaderStartCondition()||(pgm_read_byte(BOOTLOADER_ADDRESS - TINYVECTOR_RESET_OFFSET + 1)==0xff)) {
-  
-    initHardware();        
-    LED_INIT();
+
+    initHardware();
 
     if (AUTO_EXIT_NO_USB_MS>0) {
       idlePolls.b[1]=((AUTO_EXIT_MS-AUTO_EXIT_NO_USB_MS)/5)>>8;
     } else {
       idlePolls.b[1]=0;
     }
-    
-    command=cmd_local_nop;     
+
+    command=cmd_local_nop;
     currentAddress.w = 0;
-    
+
     do {
-      // 15 clockcycles per loop.     
+      // 15 clockcycles per loop.
       // adjust fastctr for 5ms timeout
-      
+
       uint16_t fastctr=(uint16_t)(F_CPU/(1000.0f*15.0f/5.0f));
       uint8_t  resetctr=100;
-  
-      do {        
+
+      do {
         if ((USBIN & USBMASK) !=0) resetctr=100;
-        
+
         if (!--resetctr) { // reset encountered
            usbNewDeviceAddr = 0;   // bits from the reset handling of usbpoll()
            usbDeviceAddr = 0;
-#if (OSCCAL_HAVE_XTAL == 0)           
-           calibrateOscillatorASM();   
-#endif           
+#if (OSCCAL_HAVE_XTAL == 0)
+           calibrateOscillatorASM();
+#endif
         }
-        
+
         if (USB_INTR_PENDING & (1<<USB_INTR_PENDING_BIT)) {
-          USB_INTR_VECTOR();  
-          USB_INTR_PENDING = 1<<USB_INTR_PENDING_BIT;  // Clear int pending, in case timeout occured during SYNC                     
+          USB_INTR_VECTOR();
+          USB_INTR_PENDING = 1<<USB_INTR_PENDING_BIT;  // Clear int pending, in case timeout occured during SYNC
           idlePolls.b[1]=0; // reset idle polls when we get usb traffic
          break;
         }
-        
-      } while(--fastctr);     
-      
+
+      } while(--fastctr);
+
       wdr();
-      
+
  #if OSCCAL_SLOW_PROGRAMMING
       osccal_tmp  = OSCCAL;
       OSCCAL      = osccal_default;
  #endif
       // commands are only evaluated after next USB transmission or after 5 ms passed
-      if (command==cmd_erase_application) 
+      if (command==cmd_erase_application)
         eraseApplication();
-      if (command==cmd_write_page) 
-        writeFlashPage();          
+      if (command==cmd_write_page)
+        writeFlashPage();
  #if OSCCAL_SLOW_PROGRAMMING
       OSCCAL      = osccal_tmp;
  #endif
-        
+
       if (command==cmd_exit) {
-        if (!fastctr) break;  // Only exit after 5 ms timeout     
+        if (!fastctr) break;  // Only exit after 5 ms timeout
       } else {
-        command=cmd_local_nop;     
-      }  
- 
+        command=cmd_local_nop;
+      }
+
       {
       // This is usbpoll() minus reset logic and double buffering
         int8_t  len;
         len = usbRxLen - 3;
-        
+
         if(len >= 0){
             usbProcessRx(usbRxBuf + 1, len); // only single buffer due to in-order processing
             usbRxLen = 0;       /* mark rx buffer as available */
         }
-        
+
         if(usbTxLen & 0x10){    /* transmit system idle */
             if(usbMsgLen != USB_NO_MSG){    /* transmit data pending? */
                 usbBuildTxBlock();
             }
         }
       }
-      
+
       idlePolls.w++;
 
-      // Try to execute program when bootloader times out      
+      // Try to execute program when bootloader times out
       if (AUTO_EXIT_MS&&(idlePolls.w==(AUTO_EXIT_MS/5))) {
          if (pgm_read_byte(BOOTLOADER_ADDRESS - TINYVECTOR_RESET_OFFSET + 1)!=0xff)  break;
       }
-      
-      LED_MACRO( idlePolls.b[0] );   
 
        // Test whether another interrupt occurred during the processing of USBpoll and commands.
-       // If yes, we missed a data packet on the bus. Wait until the bus was idle for 8.8µs to 
-       // allow synchronising to the next incoming packet. 
-       
+       // If yes, we missed a data packet on the bus. Wait until the bus was idle for 8.8µs to
+       // allow synchronising to the next incoming packet.
+
        if (USB_INTR_PENDING & (1<<USB_INTR_PENDING_BIT))  // Usbpoll() collided with data packet
-       {        
+       {
           uint8_t ctr;
-         
+
           // loop takes 5 cycles
-          asm volatile(      
-          "         ldi  %0,%1 \n\t"        
-          "loop%=:  sbis %2,%3  \n\t"        
+          asm volatile(
+          "         ldi  %0,%1 \n\t"
+          "loop%=:  sbis %2,%3  \n\t"
           "         ldi  %0,%1  \n\t"
-          "         subi %0,1   \n\t"        
-          "         brne loop%= \n\t"   
+          "         subi %0,1   \n\t"
+          "         brne loop%= \n\t"
           : "=&d" (ctr)
           :  "M" ((uint8_t)(8.8f*(F_CPU/1.0e6f)/5.0f+0.5)), "I" (_SFR_IO_ADDR(USBIN)), "M" (USB_CFG_DMINUS_BIT)
-          );       
-         USB_INTR_PENDING = 1<<USB_INTR_PENDING_BIT;                   
-       }                        
-    } while(1);  
+          );
+         USB_INTR_PENDING = 1<<USB_INTR_PENDING_BIT;
+       }
+    } while(1);
 
-    LED_EXIT();
-    
-    initHardware();  /* Disconnect micronucleus */    
-    
+    initHardware();  /* Disconnect micronucleus */
+
     USB_INTR_ENABLE = 0;
     USB_INTR_CFG = 0;       /* also reset config bits */
- 
+
   }
-   
+
   leaveBootloader();
 }
 /* ------------------------------------------------------------------------ */
